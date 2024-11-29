91b9b6331bbced9aba520bba1afffad50a4c6bd6
bkshiu1 <bkshiu1@myseneca.ca>
2024-11-30 05:27:11 +0800

    Add files via upload

---
+++ converting.h
@@ -0,0 +1,14 @@
+// CONVERTING MODULE HEADER
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void converting(void);
+
+#endif
+
+/*
+Author: Karl Shiu, bkshiu1@myseneca.ca, 131531246, 30/11/2024, CPR101, 
