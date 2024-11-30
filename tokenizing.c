e0813682ea63c5a85dfef744553a2c0ee1f23fd7
MahummadZero <132035262+MahummadZero@users.noreply.github.com>
2024-11-29 19:51:03 -0500

    Add files via upload

--- tokenizing.c
+++ tokenizing.c
@@ -1,30 +1,36 @@
-// TOKENIZING SOURCE
-#define _CRT_SECURE_NO_WARNINGS
+
+// TOKKENIZING SOURCE
+#define _CRT_SECURE_NO_WARINGS
 #define BUFFER_SIZE 300
-#include <stdio.h>
-#include <string.h>
 #include "tokenizing.h"
+/*
+Author: Mahummad Ahmed, mahmed290@myseneca, 124573239, 2024-11-29, CPR101NDD, CPR FINAL Project
+[tokenizing.c] : [CPR final Project]
+Purpose: used the string of words that are entered by the user and splits it into individual words based on spaces.
+*/
+// V1
+void tokenizing(void) {
+	printf("*** start of Tokenizing words Demo ***\n");
+	char    words[BUFFER_SIZE];
+	char* nextWord = NULL;
+	int		wordsCounter;
+	do {
+		printf("Type a few words separated by sqace (q = to quit):\n");
+		fgets(words, BUFFER_SIZE, stdin);
+		words[strlen(words) - 1] = '\0'; // Removing the newline character from fgets input
+		
+		// Tokenizing words
+		if (strcmp(words, "q") != 0) {
+			nextWord = strtok(words, " ");
+			wordsCounter = 1;
+			while (nextWord) {
+				// Displaying tokenized words
+				printf("word #%d is \'%s\'\n", wordsCounter++, nextWord);
+				nextWord = strtok(NULL, " ");
+			}
 
+		}
 
-void converting(void) {
-    printf("*** Start of Tokenizing Words Demo ***\n");
-    char words[BUFFER_SIZE];
-    char* nextWord = NULL;
-    int wordsCounter;
-
-    do {
-        printf("Type a few words separated by space (q to quit):\n");
-        fgets(words, BUFFER_SIZE, stdin);
-        words[strlen(words) - 1] = '\0'; // Remove newline character
-        if (strcmp(words, "q") != 0) {
-            nextWord = strtok(words, " ");
-            wordsCounter = 1;
-            while (nextWord) {
-                printf("word #%d is '%s'\n", wordsCounter++, nextWord);
-                nextWord = strtok(NULL, " ");
-
-            }
-        }
-    } while (strcmp(words, "q") != 0);
-    printf("*** End of Tokenizing Words Demo ***\n\n");
+	} while (strcmp(words, "q") != 0);
+	printf("*** end of Tokenizing Words Demo ***\n\n");
 }
