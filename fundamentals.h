eed439870e422b3b9f47e9755feab20b826006a6
Carl Shiu <bkshiu@mySeneca.ca>
2024-11-30 18:08:58 -0500

    Implement indexing functionality in fundamentals module

---
+++ fundamentals.h
@@ -0,0 +1,12 @@
+// FUNDAMENTALS MODULE HEADER
+#ifndef _FUNDAMENTALS_H_
+#define _FUNDAMENTALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function prototype for the fundamentals function
+void fundamentals(void);
+
+#endif
