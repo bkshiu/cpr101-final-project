748e331524da2064a654594c2e9310c0057724b6
MahummadZero <132035262+MahummadZero@users.noreply.github.com>
2024-12-01 23:03:30 -0500

    Add files via upload

--- fundamentals.c
+++ fundamentals.c
@@ -1,41 +1,42 @@
-// FUNDAMENTALS MODULE SOURCE
-#define _CRT_SECURE_NO_WARNINGS
-#define BUFFER_SIZE 80
-#define NUM_INPUT_SIZE 10
-/*
-Author: Carl Shiu, bkshiu@myseneca.ca, 127456192, 29 November 2024, CPR101NDD, Project
-[fundamental.c] : Module 1
-Purpose: [implementation of the Indexing functionality]
-*/
+// FUNDAMENTALS SOURCE
+#define _CRT_SECURE_NO_WARINGS
+#define BUFFER_SIZE		80 
+#define NUM_INPUT_SIZE  10
 #include "fundamentals.h"
+/*
+Author: Jonathan Browne, jbrowne6@myseneca.ca, ID, 2024-11-29, CPR101NDD, CPR FINAL Project
+[fundamentals.c] : [CPR final Project]
+Purpose:  is to allows the user to input one string at a specify position within the string
+*/
 
+// V1
 void fundamentals(void) {
-    printf("*** Start of Indexing Strings Demo ***\n");
+	printf("*** Start of Indexing Strings Demo ***\n");
+	// Declare variables
+	char    buffer1[BUFFER_SIZE];
+	char    numInput[NUM_INPUT_SIZE];
+	size_t  position;
+	do {
+		printf("Type not empty string (q - to quit):\n");
+		fgets(buffer1, BUFFER_SIZE, stdin);
+		buffer1[strlen(buffer1) - 1] = '\0';
+		
+		if (strcmp(buffer1, "q") != 0) { //checking if it is the exit command
+			printf("Type the character position within the string:\n");
+			fgets(numInput, NUM_INPUT_SIZE, stdin);
+			numInput[strlen(numInput) - 1] = '\0';
+			position = atoi(numInput);
+			// Check if the position is within the string's length
+			if (position >= strlen(buffer1)) {
+				position = strlen(buffer1) - 1;
+				printf("Too big... Position reduced to max. availbale\n");
+			}
+			// display the character at the specified position
 
-    char buffer1[BUFFER_SIZE];
-    char numInput[NUM_INPUT_SIZE];
-    size_t position;
-
-    do {
-        printf("Type not empty string (q - to quit):\n");
-        fgets(buffer1, BUFFER_SIZE, stdin);
-        buffer1[strlen(buffer1) - 1] = '\0'; // Remove the newline character
-
-        if (strcmp(buffer1, "q") != 0) {
-            printf("Type the character position within the string:\n");
-            fgets(numInput, NUM_INPUT_SIZE, stdin);
-            numInput[strlen(numInput) - 1] = '\0'; // Remove the newline character
-            position = atoi(numInput);
-
-            if (position >= strlen(buffer1)) {
-                position = strlen(buffer1) - 1;
-                printf("Too big... Position reduced to max. available\n");
-            }
-
-            printf("The character found at %d position is '%c'\n",
-                   (int)position, buffer1[position]);
-        }
-    } while (strcmp(buffer1, "q") != 0);
-
-    printf("*** End of Indexing Strings Demo ***\n");
+			printf("The character found at %d position is \'%c\'\n", 
+							(int)position, buffer1[position]);
+		}
+				
+	} while (strcmp(buffer1, "q") != 0);
+	printf("*** End of Indexing strings to int Demo ***\n\n");
 }
--- fundamentals.h
+++ fundamentals.h
@@ -1,12 +1,8 @@
-// FUNDAMENTALS MODULE HEADER
+// ifndef _FUNDAMENTALS_H_
 #ifndef _FUNDAMENTALS_H_
 #define _FUNDAMENTALS_H_
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-// Function prototype for the fundamentals function
+#include <stdlib.h>
 void fundamentals(void);
-
 #endif
