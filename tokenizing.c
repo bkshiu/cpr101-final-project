3eceac81d94840d1cd4a2beb73bba386f091f6c2
MahummadZero <132035262+MahummadZero@users.noreply.github.com>
2024-12-04 15:58:06 -0500

    Add files via upload
    V2 and V3

--- tokenizing.c
+++ tokenizing.c
@@ -1,6 +1,6 @@
 
 // TOKKENIZING SOURCE
-#define _CRT_SECURE_NO_WARINGS
+#define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 300
 #include "tokenizing.h"
 /*
@@ -34,3 +34,52 @@ void tokenizing(void) {
 	} while (strcmp(words, "q") != 0);
 	printf("*** end of Tokenizing Words Demo ***\n\n");
 }
+// V2
+	printf("*** start of Tokenizing phrases Demo ***\n");
+	char    phrases[BUFFER_SIZE];
+	char* nextPhrase = NULL;
+	int		phrasesCounter;
+	do {
+		printf("Type a few phrases separated by comma (q = to quit):\n");
+		fgets(phrases, BUFFER_SIZE, stdin);
+		phrase[strlen(phrases) - 1] = '\0'; // Removing the newline character from fgets input
+
+		// Tokenizing words
+		if ((strcmp(phrases, "q") != 0)) {
+			nextphrase = strtok(phrases, ",");
+			phasesCounter = 1;
+			while (nextPhrase) {
+				// Displaying tokenized words
+				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+				nextPhrase = strtok(NULL, ",");
+			}
+
+		}
+
+	} while (strcmp(words, "q") != 0);
+	printf("*** end of Tokenizing phrases Demo ***\n\n");
+
+	// V3
+	printf("*** start of Tokenizing Sentences Demo ***\n");
+	char    Sentences[BUFFER_SIZE];
+	char*	nextSentences = NULL;
+	int		SentencesCounter;
+	do {
+		printf("Type a few Sentences separated by comma (q = to quit):\n");
+		fgets(Sentences, BUFFER_SIZE, stdin);
+		Sentences[strlen(Sentences) - 1] = '\0'; // Removing the newline character from fgets input
+
+		// Tokenizing words
+		if ((strcmp(Sentences, "q") != 0)) {
+			nextSentences = strtok(Sentences, ",");
+			SentencesCounter = 1;
+			while (nextSentences) {
+				// Displaying tokenized words
+				printf("Sentences #%d is \'%s\'\n", SentencesCounter++, nextSentences);
+				nextPhrase = strtok(NULL, ",");
+			}
+
+		}
+
+	} while (strcmp(words, "q") != 0);
+	printf("*** end of Tokenizing Sentences Demo ***\n\n");
